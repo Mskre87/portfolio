18527a338ea4996f5353a7f0440e2ec5d33ef546
masackre <dimitrx.321@gmail.com>
2026-07-14 21:48:23 -0400

    feat(styles): define Bachkatov brand tokens

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html")
+
+def ui(request):
+    return render(request, "pages/ui.html")
--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("apps.core.urls")),
     path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),
 ]
 
 if settings.DEBUG:
